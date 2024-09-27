6d45deec3e4f5775672cbbcead2bff7b264ef499
Ruben Talstra <RubenTalstra1211@outlook.com>
2024-09-27 11:56:46 +0200

    looking good UI

--- bak_tracker/lib/ui/home/association_settings/association_settings_screen.dart
+++ bak_tracker/lib/ui/home/association_settings/association_settings_screen.dart
@@ -1,11 +1,13 @@
 import 'package:bak_tracker/core/themes/colors.dart';
 import 'package:bak_tracker/models/association_member_model.dart';
 import 'package:bak_tracker/services/association_service.dart';
+import 'package:bak_tracker/ui/home/approve_baks/approve_baks_screen.dart';
 import 'package:bak_tracker/ui/home/association_settings/invite_members_screen.dart';
 import 'package:bak_tracker/ui/home/association_settings/remove_members_screen.dart';
 import 'package:bak_tracker/ui/home/association_settings/permissions/list_permissions_screen.dart';
 import 'package:bak_tracker/ui/home/association_settings/update_roles_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AssociationSettingsScreen extends StatelessWidget {
   final AssociationMemberModel memberData;
@@ -27,164 +29,187 @@ class AssociationSettingsScreen extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(16.0),
         children: [
-          // Show Permissions option if the user has permission
-          if (memberData.canInviteMembers)
-            ListTile(
-              title: const Text('Invite Members'),
-              subtitle: const Text('Send invites to new members'),
-              trailing: const Icon(Icons.chevron_right),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => InviteMembersScreen(
-                      associationId: associationId,
-                    ),
-                  ),
-                );
-              },
-            ),
+          // Show Invite Members option if the user has permission
+          _buildOptionTile(
+            context,
+            icon: Icons.group_add,
+            title: 'Invite Members',
+            subtitle: 'Send invites to new members',
+            onTap: memberData.canInviteMembers
+                ? () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => InviteMembersScreen(
+                          associationId: associationId,
+                        ),
+                      ),
+                    );
+                  }
+                : null,
+          ),
           if (memberData.canInviteMembers) const Divider(),
 
           // Show Remove Members option if the user has permission
-          if (memberData.canRemoveMembers)
-            ListTile(
-              title: const Text('Remove Members'),
-              subtitle: const Text('Remove members from the association'),
-              trailing: const Icon(Icons.chevron_right),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => RemoveMembersScreen(
-                      associationId: associationId,
-                    ),
-                  ),
-                );
-              },
-            ),
+          _buildOptionTile(
+            context,
+            icon: Icons.person_remove,
+            title: 'Remove Members',
+            subtitle: 'Remove members from the association',
+            onTap: memberData.canRemoveMembers
+                ? () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => RemoveMembersScreen(
+                          associationId: associationId,
+                        ),
+                      ),
+                    );
+                  }
+                : null,
+          ),
           if (memberData.canRemoveMembers) const Divider(),
 
-          // Show List Permissions option if the user has permission
-          if (memberData.canManagePermissions)
-            ListTile(
-              title: const Text('Manage Permissions'),
-              subtitle: const Text('Update member permissions'),
-              trailing: const Icon(Icons.chevron_right),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => UpdatePermissionsScreen(
-                      associationId: associationId,
-                    ),
-                  ),
-                );
-              },
-            ),
+          // Show Manage Permissions option if the user has permission
+          _buildOptionTile(
+            context,
+            icon: Icons.admin_panel_settings,
+            title: 'Manage Permissions',
+            subtitle: 'Update member permissions',
+            onTap: memberData.canManagePermissions
+                ? () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => UpdatePermissionsScreen(
+                          associationId: associationId,
+                        ),
+                      ),
+                    );
+                  }
+                : null,
+          ),
           if (memberData.canManagePermissions) const Divider(),
 
-          // Show Update Roles option if the user has permission
-          if (memberData.canManageRoles)
-            ListTile(
-              title: const Text('Manage Roles'),
-              subtitle: const Text('Update member roles'),
-              trailing: const Icon(Icons.chevron_right),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => UpdateRolesScreen(
-                      associationId: associationId,
-                    ),
-                  ),
-                );
-              },
-            ),
+          // Show Manage Roles option if the user has permission
+          _buildOptionTile(
+            context,
+            icon: Icons.assignment_ind,
+            title: 'Manage Roles',
+            subtitle: 'Update member roles',
+            onTap: memberData.canManageRoles
+                ? () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => UpdateRolesScreen(
+                          associationId: associationId,
+                        ),
+                      ),
+                    );
+                  }
+                : null,
+          ),
           if (memberData.canManageRoles) const Divider(),
 
-          // show update bak amout option if the user has permission
-          if (memberData.canManageBaks)
-            ListTile(
-              title: const Text('Update Bak Amount'),
-              subtitle:
-                  const Text('Update the amount of baks received and consumed'),
-              trailing: const Icon(Icons.chevron_right),
-              onTap: () {
-                // Navigator.of(context).push(
-                //   MaterialPageRoute(
-                //     builder: (context) => UpdateBakAmountScreen(
-                //       associationId: associationId,
-                //     ),
-                //   ),
-                // );
-              },
-            ),
-          if (memberData.canManageBaks) const Divider(),
+          // Show Approve Baks option if the user has permission
+          _buildOptionTile(
+            context,
+            icon: FontAwesomeIcons.circleCheck,
+            title: 'Approve Baks',
+            subtitle: 'Approve or reject pending baks',
+            onTap: memberData.canApproveBaks
+                ? () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const ApproveBaksScreen(),
+                      ),
+                    );
+                  }
+                : null,
+          ),
+          if (memberData.canApproveBaks) const Divider(),
 
           // Show Reset Bak Amount option if the user has permission
-          if (memberData.canManageBaks)
-            ListTile(
-              title: Text('Reset Bak Amount',
-                  style: TextStyle(color: Colors.red.shade500)),
-              subtitle:
-                  const Text('Reset the amount of baks received and consumed'),
-              trailing: const Icon(Icons.chevron_right),
-              onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: const Text('Reset Bak Amount'),
-                      content: const Text(
-                          'Are you sure you want to reset all BAKs consumed and received? This action cannot be undone.'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            // Close the dialog without doing anything
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('Cancel',
-                              style:
-                                  TextStyle(color: AppColors.lightSecondary)),
-                        ),
-                        TextButton(
-                          onPressed: () async {
-                            try {
-                              // Call the reset method
-                              await associationService
-                                  .resetAllBaks(associationId);
-
-                              // Show success SnackBar
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content:
-                                      Text('BAKs have been reset successfully'),
-                                ),
-                              );
-                            } catch (e) {
-                              // Show error SnackBar
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text('Error: ${e.toString()}'),
-                                ),
-                              );
-                            }
-
-                            // Close the dialog after the action
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text(
-                            'Reset',
-                            style: TextStyle(
-                              color: Colors.red,
-                            ),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-            ),
+          _buildOptionTile(
+            context,
+            icon: Icons.restore,
+            title: 'Reset Bak Amount',
+            subtitle: 'Reset the amount of baks received and consumed',
+            titleStyle: TextStyle(color: Colors.red.shade500),
+            onTap: memberData.canManageBaks
+                ? () => _showResetBakDialog(context)
+                : null,
+          ),
         ],
       ),
+    );
+  }
+
+  Widget _buildOptionTile(
+    BuildContext context, {
+    required IconData icon,
+    required String title,
+    required String subtitle,
+    TextStyle? titleStyle,
+    required VoidCallback? onTap,
+  }) {
+    return ListTile(
+      leading: Icon(icon, color: AppColors.lightSecondary),
+      title: Text(title,
+          style: titleStyle ?? const TextStyle(fontWeight: FontWeight.bold)),
+      subtitle: Text(subtitle),
+      trailing: const Icon(Icons.chevron_right),
+      onTap: onTap,
+    );
+  }
+
+  void _showResetBakDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Reset Bak Amount'),
+          content: const Text(
+              'Are you sure you want to reset all BAKs consumed and received? This action cannot be undone.'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel',
+                  style: TextStyle(color: AppColors.lightSecondary)),
+            ),
+            TextButton(
+              onPressed: () async {
+                try {
+                  // Call the reset method
+                  await associationService.resetAllBaks(associationId);
+
+                  // Show success SnackBar
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('BAKs have been reset successfully'),
+                    ),
+                  );
+                } catch (e) {
+                  // Show error SnackBar
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Error: ${e.toString()}'),
+                    ),
+                  );
+                }
+
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text(
+                'Reset',
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
