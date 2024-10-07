0febd840a2e8d3ebdf56958a5bc8660eefcebe33
Ruben Talstra <RubenTalstra1211@outlook.com>
2024-10-07 15:14:44 +0200

    1.0.0+27 - possible fixed deep links for android

--- bak_tracker/android/app/src/main/kotlin/com/baktracker/MainActivity.kt
+++ bak_tracker/android/app/src/main/kotlin/com/baktracker/MainActivity.kt
@@ -3,11 +3,16 @@ package com.baktracker
 import android.os.Bundle
 import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
+import android.net.Uri
 
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         handleIntent(intent)
+        // ATTENTION: This was auto-generated to handle app links.
+        val appLinkIntent: Intent = intent
+        val appLinkAction: String? = appLinkIntent.action
+        val appLinkData: Uri? = appLinkIntent.data
     }
 
     override fun onNewIntent(intent: Intent) {
